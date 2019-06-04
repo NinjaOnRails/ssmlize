c20cb0c9666311080cbeb09b17bcf963a86afe0d
Danny Vu <danny.hvu@gmail.com>
2019-06-04 21:24:18 +0200

    Fix API key var

--- src/lib/youtube.js
+++ src/lib/youtube.js
@@ -4,6 +4,8 @@ module.exports = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    key: 'AIzaSyD9TZEQX9cK7QO1vcJlZxUPvJTkdi-XUwU',
+    key: process.env.REACT_APP_YOUTUBE_API,
   },
 });
+console.log(process.env.REACT_APP_YOUTUBE_API)
+// 'AIzaSyD9TZEQX9cK7QO1vcJlZxUPvJTkdi-XUwU'
